0b28f55b13e47995fbd3c30d8539e2de446ef572
Nianmi <Nianmi@users.noreply.github.com>
2019-12-11 18:34:10 +0100

    Add DataBase Functions

--- solution/AutoReservation.BusinessLayer.Testing/AutoUpdateTest.cs
+++ solution/AutoReservation.BusinessLayer.Testing/AutoUpdateTest.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Threading.Tasks;
+using AutoReservation.Dal.Entities;
 using AutoReservation.TestEnvironment;
 using Xunit;
 
@@ -18,10 +21,44 @@ namespace AutoReservation.BusinessLayer.Testing
         [Fact]
         public async Task UpdateAutoTest()
         {
-            throw new NotImplementedException("Test not implemented.");
             // arrange
             // act
             // assert
         }
+
+        [Fact]
+        public async Task GetAllAutos()
+        {
+            // arrange
+            Task<List<Auto>> taskList = _target.GetAll();
+            List<Auto> listAutos = taskList.Result;
+            // act
+            // assert
+            Assert.True(listAutos.Count == 4, "Count Should be 4 not: " + listAutos.Count);
+        }
+
+        [Fact]
+        public void FindAutoByPrimaryKey()
+        {
+            // arrange
+            // act
+            Auto auto = _target.GetByPrimaryKey(1);
+            // assert
+            Assert.True("Fiat Punto" == auto.Marke, "It should be Fiat Punto and not: " + auto.Marke);
+        }
+
+        [Fact]
+        public void addAuto() {
+            // arrange
+            Auto auto = new LuxusklasseAuto { Marke = "Mercedes", Tagestarif = 200 };
+
+            // act
+            _target.AddAuto(auto);
+
+            // assert
+            Auto testAuto = _target.GetByPrimaryKey(5);
+            Assert.True("Mercedes" == testAuto.Marke, "It should be Mercedes and not: " + testAuto.Marke);
+        }
+
     }
 }
--- solution/AutoReservation.BusinessLayer/AutoManager.cs
+++ solution/AutoReservation.BusinessLayer/AutoManager.cs
@@ -1,16 +1,33 @@
-﻿using System;
+﻿using AutoReservation.Dal;
+using AutoReservation.Dal.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace AutoReservation.BusinessLayer
 {
     public class AutoManager
         : ManagerBase
     {
-        // Example
-        //public async Task<List<Auto>> GetAll()
-        //{
-        //    using AutoReservationContext context = new AutoReservationContext();
-        //    return await context.Autos.ToListAsync();
-        //}
+        public async Task<List<Auto>> GetAll()
+        {
+            using AutoReservationContext context = new AutoReservationContext();
+            return await context.Autos.ToListAsync();
+        }
+
+        public Auto GetByPrimaryKey(int primaryKey) {
+            using AutoReservationContext context = new AutoReservationContext();
+            Auto foundAuto = context.Autos.Find(primaryKey);
+            return foundAuto;
+        }
+
+        public void AddAuto(Auto auto) {
+            using AutoReservationContext context = new AutoReservationContext();
+            context.Autos.Add(auto);
+            context.SaveChanges();
+
+        }
     }
 }
