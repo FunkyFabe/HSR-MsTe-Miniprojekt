b7936ceb3f6c9da5974d94eae536e83297ba7e79
Fabian Germann <fabes.germann@gmail.com>
2019-12-13 19:07:47 +0100

    rough implementation for getAuto(s) services

--- solution/AutoReservation.Service.Grpc/Services/AutoService.cs
+++ solution/AutoReservation.Service.Grpc/Services/AutoService.cs
@@ -1,3 +1,7 @@
+using System.Threading.Tasks;
+using AutoReservation.BusinessLayer;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
 using Microsoft.Extensions.Logging;
 
 namespace AutoReservation.Service.Grpc.Services
@@ -5,10 +9,23 @@ namespace AutoReservation.Service.Grpc.Services
     internal class AutoService : Grpc.AutoService.AutoServiceBase
     {
         private readonly ILogger<AutoService> _logger;
+        private AutoManager manager = new AutoManager();
 
         public AutoService(ILogger<AutoService> logger)
         {
             _logger = logger;
+        }
+
+        public override async Task<AutoDtoList> GetAutos(Empty request, ServerCallContext context)
+        {
+            var response = new AutoDtoList();
+            response.Items.AddRange(await manager.GetAll().ConvertToDtos());
+            return await Task.FromResult(response);
+        }
+
+        public override async Task<AutoDto> GetAuto(GetAutoRequest request, ServerCallContext context)
+        {
+            return await manager.GetByPrimaryKey(request.IdFilter).ConvertToDto();
         }
     }
 }
